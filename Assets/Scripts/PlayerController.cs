4cafc1ee67e42be5769820095fe6232f5fec9c0f
Abhishek-69 <60704699+Abhishek-69@users.noreply.github.com>
2021-09-27 20:32:30 +0530

    First Push

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator player;
+    public static PlayerController instance;
+    // Start is called before the first frame update
+    public CharacterController cc;
+    public PathCreator pathCreator;
+    public float speed =5;
+    float distanceTravelled,xPos;
+    [HideInInspector]public bool startRunning,pause;
+    [HideInInspector]public bool gameOver = false;
+    Vector3 desiredPos,pos;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            startRunning = false;
+            player.SetBool("run", false);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            startRunning = true;
+            xPos = Input.mousePosition.x;
+            pos = transform.position;
+        }
+        if (Input.GetMouseButton(0) )
+        {
+            float xPosDiff = (xPos - Input.mousePosition.x) / Screen.width;
+            xPosDiff *= -15;
+            desiredPos = pos + new Vector3(xPosDiff, 0, 0);
+            desiredPos.x = Mathf.Clamp(desiredPos.x, -4f, 4f);
+            distanceTravelled += speed * Time.deltaTime;
+            desiredPos.y =  pathCreator.path.GetPointAtDistance(distanceTravelled).y;
+            desiredPos.z = pathCreator.path.GetPointAtDistance(distanceTravelled).z;
+        }
+        if (startRunning && !gameOver && !pause)
+        {
+            player.SetBool("run", true);
+            cc.Move(new Vector3(desiredPos.x-transform.position.x,0, 8 * Time.deltaTime));
+            transform.position = new Vector3(transform.position.x, desiredPos.y, desiredPos.z);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public GameObject blob;
+    float Animation;
+    Vector3 blobPos, playerPos;
+    bool moveBlob, popBlob;
+    /* private void Start()
+     {
+         blobPos = blob.transform.position;
+     }*/
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.CompareTag("Cylinder"))
+        {
+            Animation = 0;
+            blob.transform.GetComponent<BoxCollider>().enabled = false;
+            blob.transform.localScale = new Vector3(1, 1, 1);
+            other.transform.GetComponent<CapsuleCollider>().enabled = false;
+            if ((transform.localScale.x == 1 && other.GetComponent<Pipe>().size == 0) ||
+                (transform.localScale.x == 1.2f && other.GetComponent<Pipe>().size == 1) ||
+                (transform.localScale.x == 1.4f && other.GetComponent<Pipe>().size == 2))
+            {
+                Debug.Log("BLOB");
+                blob.SetActive(true);
+                blob.transform.parent = transform;
+                blob.transform.localPosition = new Vector3(0, 0, 2);
+                Invoke(nameof(afterDelayBounce), .8f);
+            }
+            else
+            {
+                PlayerController.instance.player.SetBool("fall", true);
+                transform.position = new Vector3(transform.position.x, other.transform.position.y + 1, transform.position.z);
+                PlayerController.instance.gameOver = true;
+            }
+        }
+        else if (other.CompareTag("Blob") && !popBlob)
+        {
+            PlayerController.instance.pathCreator.transform.position += new Vector3(0, .8f, 0);
+            transform.localScale += new Vector3(0.2f, 0.2f, 0);
+            other.gameObject.SetActive(false);
+        }
+        else if (other.CompareTag("Obstacle"))
+        {
+            other.GetComponent<BoxCollider>().enabled = false;
+            Animation = 0;
+            blob = Instantiate(blob);
+            blob.transform.GetComponent<BoxCollider>().enabled = false;
+            blob.SetActive(true);
+            blob.transform.parent = transform;
+            blob.transform.localScale = new Vector3(1, 1, 1);
+            blob.transform.localPosition = new Vector3(0, 0, 2);
+            PlayerController.instance.pause = true;
+            PlayerController.instance.startRunning = false;
+            transform.position -= new Vector3(0, 0, 2.5f);
+            blobPos = blob.transform.position;
+            moveBlob = true;
+            popBlob = true;
+        }
+    }
+    void afterDelayBounce()
+    {
+        blobPos = blob.transform.position;
+        moveBlob = true;
+    }
+    private void Update()
+    {
+        if (moveBlob)
+        {
+            blob.transform.GetComponent<BoxCollider>().enabled = true;
+            blob.transform.localScale = new Vector3(0.5f, 0.5f, 0.6f);
+            Animation += Time.deltaTime;
+            Animation = Animation % 1.5f;
+            blob.transform.parent = null;
+            if (!popBlob)
+                blob.transform.position = MathParabola.Parabola(blobPos, blobPos + Vector3.forward * 15f, 5f, Animation / 1.5f);
+            else
+                blob.transform.position = MathParabola.Parabola(blobPos, blobPos + Vector3.right * 2f, 5f, Animation / 1.5f);
+            if (Animation >= 1.4f)
+            {
+                Debug.Log("Stop");
+                moveBlob = false;
+                popBlob = false;
+                blob.transform.position = new Vector3(blob.transform.position.x, 8, blob.transform.position.z);
+                blob.transform.GetComponent<BoxCollider>().enabled = true;
+            }
+        }
+
+    }
+}
